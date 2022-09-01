29136af0aafca09bdf8c7e2ac38104feb4404f6b
zloykoreshok <daodzen45@gmail.com>
2022-09-01 16:59:28 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("ul");
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingts = ingredients[i];
+
+  const liEl = document.createElement("li");
+  liEl.textContent = ingts;
+  liEl.classList.add("item");
+
+  elements.push(liEl);
+}
+
+ulEl.append(...elements);
