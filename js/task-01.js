1c33da44297491989cc1603a2fcde1f3a55db232
zloykoreshok <daodzen45@gmail.com>
2022-09-12 11:49:32 +0300

    task 1 Editted

--- js/task-01.js
+++ js/task-01.js
@@ -3,11 +3,11 @@ const categoriesItems = categoriesEl.children;
 console.log("Number of categories:", categoriesItems.length);
 
 const categoryElements = document.querySelectorAll(".item");
-for (let i = 0; i < categoryElements.length; i += 1) {
-  const categoryEl = categoryElements[i].children;
-  const categoryElTitle = categoryEl[0].textContent;
-  const categoryElLength = categoryEl[1].querySelectorAll("li").length;
 
-  console.log("Category:", categoryElTitle);
-  console.log("Elements:", categoryElLength);
-}
+categoryElements.forEach((item) => {
+  const categoryElTitle = item.querySelector("h2");
+  const categoryElLength = item.querySelectorAll("li");
+
+  console.log("Category:", categoryElTitle.textContent);
+  console.log("Elements:", categoryElLength.length);
+});
